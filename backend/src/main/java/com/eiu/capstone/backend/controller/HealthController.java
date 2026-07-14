8cb63d6c4e29c864e24986a23e1c66401113e376
tuankiet2005-art <tuankiet200506@gmail.com>
2026-07-14 21:30:16 +0700

    Fix: Fix conflict Heath API

--- backend/src/main/java/com/eiu/capstone/backend/controller/HealthController.java
+++ backend/src/main/java/com/eiu/capstone/backend/controller/HealthController.java
@@ -2,7 +2,6 @@ package com.eiu.capstone.backend.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +12,5 @@ public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("ok");
-=======
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HealthController {
-    @GetMapping("/health")
-    public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("Backend is active");
->>>>>>> e6c5188dadbcf5f55ff5f83f6c4ad4cfd19bb466
     }
 }
