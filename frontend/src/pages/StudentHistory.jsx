b285a9e8536cd0845b420adaca192d5013685e05
tuankiet2005-art <tuankiet200506@gmail.com>
2026-07-23 14:00:55 +0700

    feat(student): add submission history page to view past submissions

---
+++ frontend/src/pages/StudentHistory.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StudentHistoryPage from '../components/student/StudentHistoryPage';
+
+export default function StudentHistory() {
+  return <StudentHistoryPage />;
+}
