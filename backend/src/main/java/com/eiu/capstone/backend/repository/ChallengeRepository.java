470cb72855685401b2427ed83c9bca6a83d0f8e2
qkhadev <kha.pham.dev@gmail.com>
2026-07-26 22:19:14 +0700

    Refactor[Challenge]: Change Challenge entity structure

--- backend/src/main/java/com/eiu/capstone/backend/repository/ChallengeRepository.java
+++ backend/src/main/java/com/eiu/capstone/backend/repository/ChallengeRepository.java
@@ -1,11 +1,17 @@
 package com.eiu.capstone.backend.repository;
 
-import com.eiu.capstone.backend.model.Challenge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eiu.capstone.backend.model.Challenge;
+import com.eiu.capstone.backend.model.Lab;
+
 public interface ChallengeRepository extends JpaRepository<Challenge, UUID> {
-    List<Challenge> findByLab_Id(UUID labId);
+
+    Optional<Challenge> findByLabAndChallengeNumber(Lab lab, Integer challengeNumber);
+
+    List<Challenge> findByLabOrderByChallengeNumberAsc(Lab lab);
 }
